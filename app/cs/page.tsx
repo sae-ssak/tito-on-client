7b5d047e73d37acbe7314fe1a0132488bf3b3e1c
thewoowon <thewoowon@naver.com>
2024-07-22 22:34:25 +0900

    feat: cs 페이지 생성

---
+++ app/cs/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const CSPage = () => {
+  return (
+    <Container>
+      <h1>CS Page</h1>
+    </Container>
+  );
+};
+
+export default CSPage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
