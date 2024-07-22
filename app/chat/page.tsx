b84878e260074f8e8fb8cacc6e692fe921f50205
thewoowon <thewoowon@naver.com>
2024-07-22 22:34:12 +0900

    feat: chat 페이지 생성

---
+++ app/chat/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const ChatPage = () => {
+  return (
+    <Container>
+      <h1>Chat Page</h1>
+    </Container>
+  );
+};
+
+export default ChatPage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
