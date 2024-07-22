73b22fe9a92209c267c1262e44cc0ef190bdb99f
thewoowon <thewoowon@naver.com>
2024-07-22 22:34:38 +0900

    feat: fortune 페이지 생성

---
+++ app/fortune/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const FortunePage = () => {
+  return (
+    <Container>
+      <h1>Fortune Page</h1>
+    </Container>
+  );
+};
+
+export default FortunePage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
